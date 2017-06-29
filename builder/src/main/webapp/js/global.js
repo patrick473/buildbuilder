58b20df795d5455db9ffb6458737d830436f49a7
patrick <patkottman@gmail.com>
2017-06-29 22:35:53 +0200

    started building page

---
+++ builder/src/main/webapp/js/global.js
@@ -0,0 +1,9 @@
+const BASE_URI= "https://euw1.api.riotgames.com/";
+const API_KEY= "RGAPI-a0ed14fe-3092-4562-aa14-b29feab04f72";
+const CHAMPION_IMAGE="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/";
+const CHAMPION_URI= "lol/static-data/v3/champions?locale=en_US&tags=all&dataById=true";
+
+
+function loadChampions(){
+    
+}
